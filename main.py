9c27b362f3cb4434ff5bab7792d34d98c6c931a5
mr-good-bye <nikitos99099@gmail.com>
2020-05-22 01:47:41 +0300

    Added Main Thread, 2 and 5 Threads

---
+++ main.py
@@ -0,0 +1,51 @@
+from time import time
+from threading import Thread
+
+
+def print_time_main(func, b):
+    a = time()
+    func(b)
+    print("Main Thread: %.4f s" % (time()-a))
+
+
+def hard_func(a):
+    for i in range(int(a)):
+        #print(((i/a)*100)//1)
+        c = i/a
+
+
+def print_time_threading_2(func,b):
+    a = time()
+    t1 = Thread(target=func, args=(int(b//2),))
+    t2 = Thread(target=func, args=(int(b//2),))
+    t1.start()
+    t2.start()
+    t1.join()
+    t2.join()
+    print("Two Threads: %.4f s" % (time() - a))
+
+
+def print_time_threading_5(func,b):
+    a = time()
+    t1 = Thread(target=func, args=(int(b//5),))
+    t2 = Thread(target=func, args=(int(b//5),))
+    t3 = Thread(target=func, args=(int(b // 5),))
+    t4 = Thread(target=func, args=(int(b // 5),))
+    t5 = Thread(target=func, args=(int(b // 5),))
+    t1.start()
+    t2.start()
+    t3.start()
+    t4.start()
+    t5.start()
+    t1.join()
+    t2.join()
+    t3.join()
+    t4.join()
+    t5.join()
+    print("Five Threads: %.4f s" % (time() - a))
+
+
+if __name__ == "__main__":
+    print_time_main(hard_func, 200000000)
+    print_time_threading_2(hard_func, 200000000)
+    print_time_threading_5(hard_func, 200000000)
